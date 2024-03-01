e16731c3cf9fa29f4c7610b227c996f75e6ad2f2
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-03-01 20:34:08 +0000

    done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import ArticleList from "../ArticleList/ArticleList";
+import { fetchArticlesWithTopic } from "../../articles-api.js";
+import { SearchForm } from "../SearchForm/SearchForm.jsx";
+
 export default function App () {
+    
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+  const handleSearch = async (topic) => {
+    try {
+        setArticles([]);
+        setError(false);
+        setLoading(true);
+        const data = await fetchArticlesWithTopic(topic);
+        setArticles(data);
+    } catch(error) {
+        setError(true);
+    } finally {
+        setLoading(false);
+    }
+};
+
     return (
         <div>
-
+            <SearchForm onSearch={handleSearch} />
+            <h1>Latest articles</h1>
+            {loading && <p>Loading data, please wait...</p> }
+            {error && (<p>Whoops, something went wrong! Please try reloading this page!</p>
+            )}
+            {articles.length > 0 && <ArticleList items={articles}/>}
         </div>
     );
 }
