6425d5bab7cc14c295469721656884b7f4486e19
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-03-01 14:53:29 +0000

    change

---
+++ src/components/App/App.jsx
@@ -0,0 +1,22 @@
+// import Reader from "../Reader";
+// import articles from "../../articles.json";
+
+// import ClickTracker from "../ClickTracker/ClickTracker";
+// import { useState } from "react";
+// import Timer from "../Timer";
+// import css from "./App.module.css";
+
+export default function App() {
+  // const [isVisible, setIsVisible] = useState(false);
+
+  return (<div></div>
+    // <div className={css.container}>
+    //   {/* <Reader items={articles} /> */}
+    //   {/* <ClickTracker /> */}
+    //   {/* <button onClick={() => setIsVisible(!isVisible)}>
+    //     {isVisible ? "Hide" : "Show"}
+    //   </button>
+    //   {isVisible && <Timer />} */}
+    // </div>
+  );
+}
