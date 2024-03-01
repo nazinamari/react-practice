023f633fc2ee9445aa6bdca886794b0dcc05f559
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-03-01 20:36:47 +0000

    done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -34,3 +34,38 @@ export default function App () {
         </div>
     );
 }
+
+// Варіант без запитів
+
+// const App = () => {
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
+
+//   useEffect(() => {
+//     async function fetchArticles() {
+//       try {
+//         setLoading(true);
+// 			const data = await fetchArticlesWithTopic("react");
+//         setArticles(data);
+//       } catch (error) {
+//         setError(true);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+
+//     fetchArticles();
+//   }, []);
+
+// 	return (
+//     <div>
+//       <h1>Latest articles</h1>
+//       {loading && <p>Loading data, please wait...</p>}
+//       {error && (
+//         <p>Whoops, something went wrong! Please try reloading this page!</p>
+//       )}
+//       {articles.length > 0 && <ArticleList items={articles} />}
+//     </div>
+//   );
+// };
