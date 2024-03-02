55fccf05986f59119416d7fb3e6c7842daf0322b
Yevhenii Pozniak <jekamanu@gmail.com>
2024-03-02 18:59:07 +0200

    Update

---
+++ src/components/App/Planet/Planet.jsx
@@ -0,0 +1,8 @@
+export default function Planet({ name, avatar, description }) {
+  return (
+    <>
+      <h3>{name}</h3>
+      <img src={avatar} alt={description} width="200px" />
+    </>
+  );
+}
