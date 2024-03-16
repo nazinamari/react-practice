55d271a9d2ab886ed3d44baad666f09e407dab40
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-03-16 16:40:49 +0000

    comit

--- src/pages/PaymentsPage.jsx
+++ src/pages/PaymentsPage.jsx
@@ -1,19 +1,34 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getPayments } from "../payments-api"
+import PaymentList from "../components/PaymentList";
 
 export default function PaymentsPage () {
+
+    const [payments, setPayments] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false)
 
     useEffect(() => {
         async function getData() {
             try {
+                setIsLoading(true)
                 const data = await getPayments();
-            } catch (error) {}
+                setPayments(data);
+            } catch (error) {
+                setError(true)
+            } finally {
+                setIsLoading(false);
+            }
         }
-            getData();
+        getData();
     }, []);
+
     return (
         <div>
-
+            <h1>Payments</h1>
+            {isLoading && <p>Wait</p>}
+            {error && <p>Error!</p>}
+            <PaymentList payments={payments}/>
         </div>
     )
 }
