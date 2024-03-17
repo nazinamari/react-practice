0e9367a7f0620a00934b9f3f26c54509138ef10f
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-03-17 12:59:34 +0000

    filter

--- src/pages/PaymentsPage.jsx
+++ src/pages/PaymentsPage.jsx
@@ -1,12 +1,21 @@
 import { useEffect, useState } from "react"
 import { getPayments } from "../payments-api"
 import PaymentList from "../components/PaymentList";
+import { useSearchParams } from "react-router-dom";
 
 export default function PaymentsPage () {
 
     const [payments, setPayments] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(false)
+
+    const [params, setParams] = useSearchParams();
+    const ownerFilter = params.get("owner") ?? "";
+
+    const changeOwnerFilter = (newFilter) => {
+        params.set("owner", newFilter);
+        setParams(params);
+    }
 
     useEffect(() => {
         async function getData() {
@@ -27,7 +36,13 @@ export default function PaymentsPage () {
         <div>
             <h1>Payments</h1>
             <div>Filter by owner
-                <input></input>
+                <input
+                    type="text"
+                    value={ownerFilter}
+                    onChange={(e) => changeOwnerFilter(e.target.value)}
+                >
+
+                </input>
             </div>
             {isLoading && <p>Wait</p>}
             {error && <p>Error!</p>}
